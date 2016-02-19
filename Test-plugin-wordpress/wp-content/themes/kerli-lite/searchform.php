36b2c22feee7b155779a5697b9bbfa8318a5e7e2
rahmanieddy@gmail.com <rahamnieddy@gmail.com>
2016-02-19 20:42:21 +0100

    Remise a 0

--- Test-plugin-wordpress/wp-content/themes/kerli-lite/searchform.php
+++
@@ -1,5 +0,0 @@
-	<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-		<input type="search" class="search-field" placeholder="Search ..." name="s" id="s" />
-		<button type="submit"><i class="icons icon-search"></i></button>
-	</form>
-    
